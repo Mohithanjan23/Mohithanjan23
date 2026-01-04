e0804a7d6dd428dba337e42180c34c8a03bb0571
Mohith H <pes1ug23ca060@pesu.pes.edu>
2026-01-04 19:30:31 +0530

    Delete user_profile.js

--- user_profile.js
+++
@@ -1,9 +0,0 @@
-const mohith = {
-  pronouns: "He/Him",
-  role: "Full Stack Developer",
-  code: ["Javascript", "Python", "Dart", "HTML/CSS"],
-  tools: ["React", "Flutter", "Node.js", "Figma"],
-  architectures: ["Microservices", "Serverless"],
-  currentFocus: "Building OpenMango & Ideavore 🚀",
-  challenge: "Bridging complex Backend with clean UI/UX"
-};
